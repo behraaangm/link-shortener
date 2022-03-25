a44938567f5308fa1b7c7672b385c1c63b9e9530
behraaangm <behraaangm@gmail.com>
2022-03-25 00:31:36 -0500

    make create action render error on nvalid urls and a full short url on link model

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,6 +3,11 @@ class LinksController < ApplicationController
   def create
     shortener = Shortener.new(link_params[:original_url])
     @link = shortener.create_link
+    if @link.persisted?
+
+    else
+      render 'error.js.erb'
+    end
   end
 
   private
